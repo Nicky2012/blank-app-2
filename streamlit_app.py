e1f2df646946d60dd66328ae3944ed170a47ada3
Nicky2012 <dangnguyen141012@gmail.com>
2024-09-15 14:59:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ with st.container():
   st.subheader("My name is Nicky :wave:")
   st.title('About me :smile:')
   st.write(" At just 11 years old, you have a vibrant passion for both coding and sports, showcasing a dynamic blend of interests. Hailing from Vietnam, you balance the intellectual challenge of programming with the physical excitement of athletic activities. Whether you're debugging lines of code or scoring goals on the field, your enthusiasm for these pursuits highlights a well-rounded and energetic approach to life. Your unique combination of skills and interests sets you apart, promising a future filled with creativity and athleticism.")
-  st.title('My country:")
+  st.title("My country:")
   st.write('
   Vietnam is a vibrant country known for its stunning landscapes, rich cultural heritage, and delicious cuisine.')
   st.write("[Learn more >](https://en.wikipedia.org/wiki/Vietnam)")
